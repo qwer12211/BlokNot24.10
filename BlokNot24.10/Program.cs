c019bf12eea3f223c369f624babe1c0866e089f9
qwer12211 <t4475434@gmail.com>
2023-10-24 20:25:55 +0300

    Добавьте файлы проекта.

---
+++ BlokNot24.10/Program.cs
@@ -0,0 +1,44 @@
+﻿List<string> notes = new List<string>
+{
+    "Заметка 1",
+    "Заметка 2",
+    "Заметка 3",
+    "Заметка 4",
+    "Заметка 5"
+};
+Console.WriteLine("Выбранная дата");
+Console.WriteLine("  Заметка 1");
+Console.WriteLine("  Заметка 2");
+Console.WriteLine("  Заметка 3");
+Console.WriteLine("  Заметка 4");
+Console.WriteLine("  Заметка 5");
+int pos = 1;
+ConsoleKeyInfo key;
+Console.CursorVisible = false;
+
+do
+{
+    key = Console.ReadKey();
+
+    Console.SetCursorPosition(0, pos);
+    Console.WriteLine("  ");
+
+    if (key.Key == ConsoleKey.UpArrow && pos != 1)
+    {
+        pos--;
+    }
+    else if (key.Key == ConsoleKey.DownArrow && pos != 5)
+    {
+        pos++;
+    }
+    Console.SetCursorPosition(0, pos);
+    Console.WriteLine("->");
+
+} while (key.Key != ConsoleKey.Enter);
+Console.Clear();
+Console.WriteLine("Выбранная дата");
+Console.WriteLine("-----------------------");
+Console.WriteLine("  " + notes[pos - 1]); 
+Console.WriteLine("-----------------------");
+Console.WriteLine("Описание: ");
+Console.WriteLine("Дата: ");
