b38dfaee3daa12323a716198c738b7c4f4c543b9
qwer12211 <145342155+qwer12211@users.noreply.github.com>
2023-10-30 12:56:23 +0300

    Update Program.cs

--- BlokNot24.10/Program.cs
+++ BlokNot24.10/Program.cs
@@ -1,44 +1,112 @@
-﻿List<string> notes = new List<string>
+using System;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
 {
-    "Заметка 1",
-    "Заметка 2",
-    "Заметка 3",
-    "Заметка 4",
-    "Заметка 5"
-};
-Console.WriteLine("Выбранная дата");
-Console.WriteLine("  Заметка 1");
-Console.WriteLine("  Заметка 2");
-Console.WriteLine("  Заметка 3");
-Console.WriteLine("  Заметка 4");
-Console.WriteLine("  Заметка 5");
-int pos = 1;
-ConsoleKeyInfo key;
-Console.CursorVisible = false;
+    private static List<Note> notes = new List<Note>();
+    private static int poz = 0;
+    static void Main(string[] args)
+    {
+        Allnotes();
+        ConsoleKeyInfo key;
 
-do
+        do
+        {
+            Console.Clear();
+            Menu();
+
+            key = Console.ReadKey();
+            DateTime currentDate = DateTime.Now;
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (poz > 0)
+                        poz--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (poz < notes.Count - 1)
+                        poz++;
+                    break;
+                case ConsoleKey.Enter:
+                    SND(notes[poz]);
+                    break;
+                case ConsoleKey.LeftArrow:
+                    currentDate = currentDate.AddDays(-1); // не работает переключение дат, почему не знаю
+                    break;
+                case ConsoleKey.RightArrow:
+                    currentDate = currentDate.AddDays(1);
+                    break;
+            }
+        } while (key.Key != ConsoleKey.Escape);
+    }
+
+    private static void Menu()
+    {
+        Console.WriteLine($"Дата: {DateTime.Now}"); 
+        Console.WriteLine();
+
+        for (int i = 0; i < notes.Count; i++)
+        {
+            Console.Write(i == poz ? "-> " : "  ");
+            Console.WriteLine(notes[i].Naznanie);
+        }
+    }
+    private static void Allnotes()
+    {
+        notes.Add(new Note
+        {
+            Naznanie = "Придти на пары",
+            Opisanie = "Отсидеть пять пар",
+            Date = new DateTime(2023, 12, 12), // не понимаю как сделать унивесальную дату
+
+        });
+
+        notes.Add(new Note
+        {
+            Naznanie = "Сходить в магазин",
+            Opisanie = "Купить хлеба",
+            Date = new DateTime(2023, 12, 12),
+
+        });
+
+        notes.Add(new Note
+        {
+            Naznanie = "Пойти погулять",
+            Opisanie = "Прогуляться по парку",
+            Date = new DateTime(2023, 12, 12),
+
+        });
+
+        notes.Add(new Note
+        {
+            Naznanie = "Сделать уроки",
+            Opisanie = "Выполнить практические работы",
+            Date = new DateTime(2023, 12, 12),
+        });
+
+        notes.Add(new Note
+        {
+            Naznanie = "Придти домой",
+            Opisanie = "отдохнуть",
+            Date = new DateTime(2023, 12, 12), 
+        });
+    }
+    private static void SND(Note note)
+    {
+        Console.Clear();
+        Console.WriteLine($"Название: {note.Naznanie}");
+        Console.WriteLine("-----------------------------------");
+        Console.WriteLine($"Описание: {note.Opisanie}");
+        Console.WriteLine($"Дата: {DateTime.Now}");
+        Console.WriteLine("\nНажмите любую клавишу для возврата.");
+        Console.ReadKey();
+    }
+}
+class Note
 {
-    key = Console.ReadKey();
-
-    Console.SetCursorPosition(0, pos);
-    Console.WriteLine("  ");
-
-    if (key.Key == ConsoleKey.UpArrow && pos != 1)
-    {
-        pos--;
-    }
-    else if (key.Key == ConsoleKey.DownArrow && pos != 5)
-    {
-        pos++;
-    }
-    Console.SetCursorPosition(0, pos);
-    Console.WriteLine("->");
-
-} while (key.Key != ConsoleKey.Enter);
-Console.Clear();
-Console.WriteLine("Выбранная дата");
-Console.WriteLine("-----------------------");
-Console.WriteLine("  " + notes[pos - 1]); 
-Console.WriteLine("-----------------------");
-Console.WriteLine("Описание: ");
-Console.WriteLine("Дата: ");
+    public string Naznanie { get; set; }
+    public string Opisanie { get; set; }
+    public DateTime Date { get; set; }
+    public bool IsCompleted { get; set; }
+}
